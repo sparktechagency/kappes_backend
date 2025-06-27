694578f8f30e04d4fa93db013379d74b092048ca
zn-rabby <rabby.webdeveloper@gmail.com>
2025-06-27 15:33:44 +0600

    feat: add real-time notification sending and saving to DB

---
+++ src/utils/notificaton.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-undef */
+import { INotification } from '../app/modules/notification/notification.interface';
+import { Notification } from '../app/modules/notification/notification.model';
+
+export const sendNotifications = async (data: any): Promise<INotification> => {
+     const result = await Notification.create(data);
+     console.log('notifications=', result);
+     //@ts-ignore
+     const socketIo = global.io;
+     if (socketIo) {
+          socketIo.emit(`notification::${data?.receiver}`, result);
+     }
+
+     return result;
+};
