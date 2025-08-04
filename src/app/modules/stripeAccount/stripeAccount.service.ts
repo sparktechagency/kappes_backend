23bbdb26f2c352fa744484a43b9f63fbf53c0da6
asifaowadud <asifaowadud@gmail.com>
2025-08-04 19:27:07 +0600

    stripe included

--- src/app/modules/stripeAccount/stripeAccount.service.ts
+++ src/app/modules/stripeAccount/stripeAccount.service.ts
@@ -6,65 +6,6 @@ import { User } from '../user/user.model';
 import { StripeAccount } from './stripeAccount.model';
 import { successHTMLstripeConnection } from './stripeAccount.utils';
 
-// const createStripeAccount = async (
-//     user: any,
-//     host: string,
-//     protocol: string,
-// ): Promise<any> => {
-//     // console.log('user',user);
-//     const existingAccount = await StripeAccount.findOne({
-//         userId: user.userId,
-//     }).select('user accountId isCompleted');
-//     // console.log('existingAccount', existingAccount);
-//     console.log({ user, host, protocol });
-//     console.log(existingAccount);
-
-//     if (existingAccount) {
-//         if (existingAccount.isCompleted) {
-//             return {
-//                 success: false,
-//                 message: 'Account already exists',
-//                 data: existingAccount,
-//             };
-//         }
-
-//         const onboardingLink = await stripe.accountLinks.create({
-//             account: existingAccount.accountId,
-//             refresh_url: `${protocol}://${host}/api/v1/stripe/refreshAccountConnect/${existingAccount.accountId}`,
-//             return_url: `${protocol}://${host}/api/v1/stripe/success-account/${existingAccount.accountId}`,
-//             type: 'account_onboarding',
-//         });
-//         // console.log('onboardingLink-1', onboardingLink);
-
-//         return onboardingLink.url;
-//     }
-
-//     const account = await stripe.accounts.create({
-//         type: 'express',
-//         email: user.email,
-//         country: 'US',
-//         capabilities: {
-//             card_payments: { requested: true },
-//             transfers: { requested: true },
-//         },
-//     });
-//     // console.log('stripe account', account);
-
-//     await StripeAccount.create({ accountId: account.id, userId: user.userId });
-//     // save stripe account id in user
-//     await User.findByIdAndUpdate(user.userId, { $set: { stripeConnectedAccount: account.id } });
-
-//     const onboardingLink = await stripe.accountLinks.create({
-//         account: account.id,
-//         refresh_url: `${protocol}://${host}/api/v1/stripe/refreshAccountConnect/${account.id}`,
-//         return_url: `${protocol}://${host}/api/v1/stripe/success-account/${account.id}`,
-//         type: 'account_onboarding',
-//     });
-//     console.log('onboardingLink-2', onboardingLink);
-
-//     return onboardingLink.url;
-// };
-
 const createConnectedStripeAccount = async (user: IJwtPayload, host: string, protocol: string): Promise<any> => {
      const existingAccount = await StripeAccount.findOne({
           user: user.id,
--- src/app/modules/wishlist/wishlist.service.ts
+++ src/app/modules/wishlist/wishlist.service.ts
@@ -1,64 +1,54 @@
+import { StatusCodes } from 'http-status-codes';
 import { Types } from 'mongoose';
+import AppError from '../../../errors/AppError';
+import QueryBuilder from '../../builder/QueryBuilder';
 import { IWishlist, IWishlistItem } from './wishlist.interface';
 import { Wishlist } from './wishlist.model';
-import AppError from '../../../errors/AppError';
-import { StatusCodes } from 'http-status-codes';
-import { Record } from 'aws-sdk/clients/cognitosync';
-import QueryBuilder from '../../builder/QueryBuilder';
 
 const addToWishlist = async (userId: string, productId: string): Promise<IWishlist> => {
-    let wishlist = await Wishlist.findOne({ user: userId });
+     let wishlist = await Wishlist.findOne({ user: userId });
 
-    if (!wishlist) {
-        wishlist = await Wishlist.create({
-            user: userId,
-            items: [{ product: productId }],
-        });
-    } else {
-        const itemExists = wishlist.items.some((item: IWishlistItem) =>
-            item.product.toString() === productId
-        );
+     if (!wishlist) {
+          wishlist = await Wishlist.create({
+               user: userId,
+               items: [{ product: productId }],
+          });
+     } else {
+          const itemExists = wishlist.items.some((item: IWishlistItem) => item.product.toString() === productId);
 
-        if (itemExists) {
-            throw new AppError(StatusCodes.BAD_REQUEST, 'Product already in wishlist');
-        }
+          if (itemExists) {
+               throw new AppError(StatusCodes.BAD_REQUEST, 'Product already in wishlist');
+          }
 
-        wishlist.items.push({ product: new Types.ObjectId(productId) });
-        await wishlist.save();
-    }
+          wishlist.items.push({ product: new Types.ObjectId(productId) });
+          await wishlist.save();
+     }
 
-    return wishlist.populate('items.product');
+     return wishlist.populate('items.product');
 };
 
 const removeFromWishlist = async (userId: string, productId: string): Promise<IWishlist | null> => {
-    const wishlist = await Wishlist.findOneAndUpdate(
-        { user: userId },
-        { $pull: { items: { product: productId } } },
-        { new: true }
-    ).populate('items.product');
+     const wishlist = await Wishlist.findOneAndUpdate({ user: userId }, { $pull: { items: { product: productId } } }, { new: true }).populate('items.product');
 
-    return wishlist;
+     return wishlist;
 };
 
 const getWishlist = async (userId: string, query: any) => {
-    const querBuilder = new QueryBuilder(Wishlist.find({ user: userId }).populate('items.product'), query);
+     const querBuilder = new QueryBuilder(Wishlist.find({ user: userId }).populate('items.product'), query);
 
-    const result = await querBuilder.fields().sort().paginate().filter().search(['items.product.name', 'items.product.description', 'items.product.price']).modelQuery; // Final query model    
+     const result = await querBuilder.fields().sort().paginate().filter().search(['items.product.name', 'items.product.description', 'items.product.price']).modelQuery; // Final query model
 
-    const meta = await querBuilder.countTotal();
-    return { result, meta };
+     const meta = await querBuilder.countTotal();
+     return { result: result[0], meta };
 };
 
 const isProductInWishlist = async (userId: string, productId: string): Promise<boolean> => {
-    return Wishlist.isProductInWishlist(
-        new Types.ObjectId(userId),
-        new Types.ObjectId(productId)
-    );
+     return Wishlist.isProductInWishlist(new Types.ObjectId(userId), new Types.ObjectId(productId));
 };
 
 export const WishlistService = {
-    addToWishlist,
-    removeFromWishlist,
-    getWishlist,
-    isProductInWishlist,
+     addToWishlist,
+     removeFromWishlist,
+     getWishlist,
+     isProductInWishlist,
 };
