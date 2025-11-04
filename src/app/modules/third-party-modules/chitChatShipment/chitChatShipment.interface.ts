7430a8b26762af746b54f70b020beb0dfd24e339
asifaowadud <asifaowadud@gmail.com>
2025-11-04 11:07:54 +0600

    feat(chitChatShipment): interface,model,validation for shipment creation ready

--- src/app/modules/third-party-modules/chitChatShipment/chitChatShipment.interface.ts
+++ src/app/modules/third-party-modules/chitChatShipment/chitChatShipment.interface.ts
@@ -1,8 +1,63 @@
+import {
+     chitChatShipment_order_store,
+     chitChatShipment_package_contents,
+     chitChatShipment_package_type,
+     chitChatShipment_postage_type,
+     chitChatShipment_size_unit,
+     chitChatShipment_value_currency,
+     chitChatShipment_weight_unit,
+} from './chitChatShipment.enum';
+
 export interface IchitChatShipment {
-     createdAt: Date;
-     updatedAt: Date;
+     name: string;
+     address_1: string;
+     address_2?: string;
+     city: string;
+     province_code?: string;
+     postal_code?: string;
+     country_code: string;
+     phone?: string;
+     email?: string;
+     return_name?: string;
+     return_address_1?: string;
+     return_address_2?: string;
+     return_city?: string;
+     return_province_code?: string;
+     return_phone?: string;
+     package_contents?: chitChatShipment_package_contents;
+     description?: string;
+     value: string;
+     value_currency: chitChatShipment_value_currency;
+     order_id?: string;
+     order_store?: chitChatShipment_order_store;
+     package_type: chitChatShipment_package_type;
+     weight_unit: chitChatShipment_weight_unit;
+     weight: number;
+     size_unit: chitChatShipment_size_unit;
+     size_x: number;
+     size_y: number;
+     size_z: number;
+     insurance_requested?: boolean;
+     signature_requested?: boolean;
+     vat_reference?: string;
+     duties_paid_requested?: boolean;
+     postage_type?: chitChatShipment_postage_type;
+     cheapest_postage_type_requested?: string;
+     tracking_number?: string;
+     ship_date?: string;
+     line_items?: {
+          quantity: number;
+          description: string;
+          value_amount: string;
+          currency_code: string;
+          hs_tariff_code: string;
+          sku_code: string;
+          origin_country: string;
+          weight: number;
+          weight_unit: string;
+     }[];
      isDeleted: boolean;
-     deletedAt?: Date;
+     deletedAt: Date;
 }
 
 export type IchitChatShipmentFilters = {
--- src/app/modules/third-party-modules/chitChatShipment/chitChatShipment.model.ts
+++ src/app/modules/third-party-modules/chitChatShipment/chitChatShipment.model.ts
@@ -1,10 +1,149 @@
 import { Schema, model } from 'mongoose';
 import { IchitChatShipment } from './chitChatShipment.interface';
+import {
+     chitChatShipment_order_store,
+     chitChatShipment_package_contents,
+     chitChatShipment_package_type,
+     chitChatShipment_postage_type,
+     chitChatShipment_size_unit,
+     chitChatShipment_value_currency,
+     chitChatShipment_weight_unit,
+} from './chitChatShipment.enum';
 
-const ChitChatShipmentSchema = new Schema<IchitChatShipment>({
-     isDeleted: { type: Boolean, default: false },
-     deletedAt: { type: Date },
-}, { timestamps: true });
+const ChitChatShipmentSchema = new Schema<IchitChatShipment>(
+     {
+          name: {
+               type: String,
+               required: true,
+          },
+          address_1: {
+               type: String,
+               required: true,
+          },
+          address_2: String,
+          city: {
+               type: String,
+               required: true,
+          },
+          province_code: String,
+          postal_code: String,
+          country_code: {
+               type: String,
+               required: true,
+          },
+          phone: String,
+          email: String,
+          return_name: String,
+          return_address_1: String,
+          return_address_2: String,
+          return_city: String,
+          return_province_code: String,
+          return_phone: String,
+          package_contents: {
+               type: String,
+               enum: chitChatShipment_package_contents,
+          },
+          description: String,
+          value: {
+               type: String,
+               required: true,
+          },
+          value_currency: {
+               type: String,
+               enum: chitChatShipment_value_currency,
+          },
+          order_id: String,
+          order_store: {
+               type: String,
+               enum: chitChatShipment_order_store,
+          },
+          package_type: {
+               type: String,
+               enum: chitChatShipment_package_type,
+               required: true,
+          },
+          weight_unit: {
+               type: String,
+               enum: chitChatShipment_weight_unit,
+               required: true,
+          },
+          weight: {
+               type: Number,
+               required: true,
+          },
+          size_unit: {
+               type: String,
+               enum: chitChatShipment_size_unit,
+               required: true,
+          },
+          size_x: {
+               type: Number,
+               required: true,
+          },
+          size_y: {
+               type: Number,
+               required: true,
+          },
+          size_z: {
+               type: Number,
+               required: true,
+          },
+          insurance_requested: Boolean,
+          signature_requested: Boolean,
+          vat_reference: String,
+          duties_paid_requested: Boolean,
+          postage_type: {
+               type: String,
+               enum: chitChatShipment_postage_type,
+          },
+          cheapest_postage_type_requested: String,
+          tracking_number: String,
+          ship_date: String,
+          line_items: [
+               {
+                    quantity: {
+                         type: Number,
+                         required: true,
+                    },
+                    description: {
+                         type: String,
+                         required: true,
+                    },
+                    value_amount: {
+                         type: String,
+                         required: true,
+                    },
+                    currency_code: {
+                         type: String,
+                         required: true,
+                    },
+                    hs_tariff_code: {
+                         type: String,
+                         required: true,
+                    },
+                    sku_code: {
+                         type: String,
+                         required: true,
+                    },
+                    origin_country: {
+                         type: String,
+                         required: true,
+                    },
+                    weight: {
+                         type: Number,
+                         required: true,
+                    },
+                    weight_unit: {
+                         type: String,
+                         required: true,
+                    },
+               },
+          ],
+          isDeleted: { type: Boolean, default: false },
+          deletedAt: { type: Date },
+     },
+     { timestamps: true },
+);
 
 ChitChatShipmentSchema.pre('find', function (next) {
      this.find({ isDeleted: false });
@@ -19,6 +158,6 @@ ChitChatShipmentSchema.pre('findOne', function (next) {
 ChitChatShipmentSchema.pre('aggregate', function (next) {
      this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
      next();
-});       
+});
 
 export const ChitChatShipment = model<IchitChatShipment>('ChitChatShipment', ChitChatShipmentSchema);
